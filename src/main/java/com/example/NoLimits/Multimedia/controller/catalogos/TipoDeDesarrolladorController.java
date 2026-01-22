7429ac5b9efb7ac16cf9403c731e31cef7b45de1
SimonKaiak <ch.troncoso@duocuc.cl>
2026-01-22 00:15:51 -0300

    v1 En TipoDeDesarrolladorController

--- src/main/java/com/example/NoLimits/Multimedia/controller/catalogos/TipoDeDesarrolladorController.java
+++ src/main/java/com/example/NoLimits/Multimedia/controller/catalogos/TipoDeDesarrolladorController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/tipos-desarrollador")
+@RequestMapping("/api/v1/tipos-desarrollador")
 public class TipoDeDesarrolladorController {
 
     @Autowired
